8f105da01a603582b82419e90ff452347066be35
wjdwl002 <wjdwl001@khu.ac.kr>
2021-09-06 21:07:40 +0900

    login 위치 조정

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
+const USERNAME_KEY = "username"
+const HIDDEN_CLASSNAME = "hidden"
+
+
 const loginForm = document.getElementById("login_form");
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+const greetingElement = document.getElementById("greeting");
 
-const HIDDEN_CLASSNAME = "hidden"
+if(savedUsername != undefined) {
+    greetingElement.classList.remove("hidden");
+    greetingElement.innerHTML += savedUsername;
+} else {
+    loginForm.classList.remove("hidden");
+}
+
+
 
 function onLoginSubmit(event){
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem('username', username);
-    console.log(username);
-    const greeting = document.getElementById("greeting");
-    greeting.innerHTML += username;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    greetingElement.innerHTML += username;
+    greetingElement.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit",onLoginSubmit);
